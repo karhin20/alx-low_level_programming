6ad5a5437e33ffa6708d51df864862e3126930b3
karhin20 <kelvinarhin20@gmail.com>
2023-02-16 15:24:16 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <std.io>
+/**
+ * main - A c program that prints a line with puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
