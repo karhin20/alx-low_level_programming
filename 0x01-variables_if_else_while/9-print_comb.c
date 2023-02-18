1123ef57adadd2b927fa1e7a27cd8a843a0a756f
karhin20 <kelvinarhin20@gmail.com>
2023-02-17 20:43:31 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,17 @@
 
 int main(void)
 {
-	int n = 0,
+	int n = 0;
+
 	while (n < 10)
 	{
 		putchar(48 + n);
-		if (c != 9)
+		if (n != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 		n++;
 	}
-	return (0)
+	return (0);
 }
