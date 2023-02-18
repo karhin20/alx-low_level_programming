0c2574b5311241b3e1f966209aa762978d24d424
karhin20 <kelvinarhin20@gmail.com>
2023-02-17 17:15:05 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns single digits numbers of base 10 numbers from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		if (n <= 9)
+		{
+			putchar(n + '0');
+		}
+		n++;
+	}
+	putchar('\n';)
+	return (0);
+}
+
+
+
