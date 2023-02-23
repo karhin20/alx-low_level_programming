e86e2ac94c8ebaf46f86c9832c01ba1ee352d53d
karhin20 <kelvinarhin20@gmail.com>
2023-02-23 06:46:37 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: variable to be checked
+ *
+ * Return: 1 for  uppercase
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
