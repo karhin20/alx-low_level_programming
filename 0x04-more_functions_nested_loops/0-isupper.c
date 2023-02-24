7f1d20a45bf01d4847075defc120d0b00db96a38
karhin20 <kelvinarhin20@gmail.com>
2023-02-23 18:46:49 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  *
  * @c: variable to be checked
  *
- * Return: 1 for  uppercase
+ * Return: Always 0
  */
 
 int _isupper(int c)
@@ -13,10 +13,12 @@ int _isupper(int c)
 
 if (c >= 'A' && c <= 'Z')
 {
-	return (1);
+return (1);
 }
 else
 {
-	return (0);
+return (0);
 }
+
 }
+
