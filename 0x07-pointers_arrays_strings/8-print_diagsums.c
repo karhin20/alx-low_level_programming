00a6cb2adb363b728715c5a9f9ad5d3cd46564b5
karhin20 <kelvinarhin20@gmail.com>
2023-03-06 17:11:47 -0800

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+
