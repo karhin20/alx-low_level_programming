d6d2eb6f4af8b79a4f64ce2d432de7380cc1d5a3
karhin20 <kelvinarhin20@gmail.com>
2023-03-08 22:00:48 -0800

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,21 +12,21 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (find_prime(n, n - 1));
+	return (actual_prime(n, n - 1));
 }
 
 /**
- * find_prime - calculates if a number is prime recursively
+ * actual_prime - calculates if a number is prime recursively
  * @n: number to evaluate
  * @i: iterator
  *
  * Return: 1 if n is prime, 0 if not
  */
-int find_prime(int n, int i)
+int actual_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (find_prime(n, i - 1));
+	return (actual_prime(n, i - 1));
 }
